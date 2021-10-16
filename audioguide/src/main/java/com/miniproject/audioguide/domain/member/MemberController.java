b2d2d61f01a3c928c3319cfeb13ba3f60c461234
planetdoy <doydoit@gmail.com>
2021-10-16 17:56:09 +0900

    feat : 로그아웃 API 추가

--- audioguide/src/main/java/com/miniproject/audioguide/domain/member/MemberController.java
+++ audioguide/src/main/java/com/miniproject/audioguide/domain/member/MemberController.java
@@ -1,12 +1,14 @@
 package com.miniproject.audioguide.domain.member;
 
 import com.miniproject.audioguide.common.Sha256;
+import com.miniproject.audioguide.exception.NoSessionException;
 import com.miniproject.audioguide.exception.NotMatchException;
 import com.miniproject.audioguide.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,4 +59,21 @@ public class MemberController {
             throw new NotMatchException("Fail to login, Check your ID or PASSWORD");
         }
     }
+
+    /**
+     * 로그아웃 api
+     * @return
+     */
+    @GetMapping("/api/members/logout")
+    public ResponseEntity logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new NoSessionException("Have No Session, Login First");
+        }
+        String loginId = (String) session.getAttribute(SessionConst.Login_Member);
+
+        session.invalidate();
+        return new ResponseEntity(String.format("%s logout",loginId),HttpStatus.OK);
+    }
+
 }
