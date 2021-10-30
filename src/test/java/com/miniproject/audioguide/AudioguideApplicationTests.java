3431577d2b401787ee3b26fccfe908cc2596f545
planetdoy <doydoit@gmail.com>
2021-10-30 17:03:21 +0900

    build : test 설정 수정

--- src/test/java/com/miniproject/audioguide/AudioguideApplicationTests.java
+++ src/test/java/com/miniproject/audioguide/AudioguideApplicationTests.java
@@ -3,7 +3,7 @@ package com.miniproject.audioguide;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties={"spring.config.location=/home/ec2-user/app/application-real-db.yml"})
 class AudioguideApplicationTests {
 
 	@Test
