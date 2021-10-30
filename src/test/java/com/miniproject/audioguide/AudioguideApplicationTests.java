1b99c29198bdfc76d8e5694bd9627c7911ab2db9
planetdoy <doydoit@gmail.com>
2021-10-30 17:36:43 +0900

    build : test 설정 수정

--- src/test/java/com/miniproject/audioguide/AudioguideApplicationTests.java
+++ src/test/java/com/miniproject/audioguide/AudioguideApplicationTests.java
@@ -2,8 +2,10 @@ package com.miniproject.audioguide;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties={"spring.config.location=/home/ec2-user/app/application-real-db.yml"})
+@ActiveProfiles(profiles = {"real"})
+@SpringBootTest
 class AudioguideApplicationTests {
 
 	@Test
