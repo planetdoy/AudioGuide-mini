d6286b1af6aa4db7bb78f4e7dfcd7cb64dcdb82f
planetdoy <doydoit@gmail.com>
2021-10-30 16:12:05 +0900

    fix : 테스트 코드 설정

--- src/test/java/com/miniproject/audioguide/AudioguideApplicationTests.java
+++ src/test/java/com/miniproject/audioguide/AudioguideApplicationTests.java
@@ -3,7 +3,7 @@ package com.miniproject.audioguide;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.config=application-real-db.yml"})
 class AudioguideApplicationTests {
 
 	@Test
