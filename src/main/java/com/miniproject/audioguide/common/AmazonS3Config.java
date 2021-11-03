64a9c3f2e31e203078f7fa162015ccf702f727ce
planetdoy <doydoit@gmail.com>
2021-11-03 11:57:23 +0900

    fix : 의존성 주입 시점에 따른 @PostConstruct 사용

--- src/main/java/com/miniproject/audioguide/common/AmazonS3Config.java
+++ src/main/java/com/miniproject/audioguide/common/AmazonS3Config.java
@@ -5,32 +5,28 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+import javax.annotation.PostConstruct;
+
+@Component
 public class AmazonS3Config {
+    private AmazonS3Client amazonS3Client;
 
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
+    @Value("${cloud.aws.region.static}")
     private String region;
 
-    public AmazonS3Config(
-            @Value("${cloud.aws.credentials.accessKey}") String accessKey,
-            @Value("${cloud.aws.credentials.secretKey}") String secretKey,
-            @Value("${cloud.aws.region.static}") String region
-    ) {
-        this.accessKey = accessKey;
-        this.secretKey = secretKey;
-        this.region = region;
-    }
+    @PostConstruct
+    public void setAmazonS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
 
-    @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+        amazonS3Client = (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(this.region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
     }
 }
