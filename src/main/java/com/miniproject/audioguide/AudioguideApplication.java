7f4d25cf2a5dcefa5ca2988b0bd046fed1e974a7
planetdoy <doydoit@gmail.com>
2021-12-09 16:49:50 +0900

    fix : propeties 오류 수정

--- src/main/java/com/miniproject/audioguide/AudioguideApplication.java
+++ src/main/java/com/miniproject/audioguide/AudioguideApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AudioguideApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		String getVersion = org.springframework.core.SpringVersion.getVersion();
+		System.out.println("spring framework version : "+getVersion);
 		SpringApplication.run(AudioguideApplication.class, args);
 	}
-
 }
